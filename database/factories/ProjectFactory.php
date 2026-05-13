f1d7297869b0c06d64eedd8b876af111ec88969d
alson loquinario <asloquinario@antiquespride.edu.ph>
2026-05-13 13:15:03 +0800

    fix: fix migration file

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -20,15 +20,15 @@ class ProjectFactory extends Factory
             'Kapital Access for Young Agripreneurs (KAYA)',
         ];
 
-        $start = $this->faker->dateTimeBetween('-3 years', '-6 months');
+        $start = fake()->dateTimeBetween('-3 years', '-6 months');
 
         return [
-            'project_name' => $this->faker->randomElement($projects),
-            'project_code' => 'SLP-'.strtoupper($this->faker->unique()->bothify('??###')),
-            'description' => $this->faker->sentence(12),
+            'project_name' => fake()->randomElement($projects),
+            'project_code' => 'SLP-'.strtoupper(fake()->unique()->bothify('??###')),
+            'description' => fake()->sentence(12),
             'date_started' => $start->format('Y-m-d'),
-            'date_ended' => $this->faker->optional(0.4)->dateTimeBetween($start, 'now')?->format('Y-m-d'),
-            'fund_source' => $this->faker->randomElement(['DSWD Central Office', 'LGU', 'DSWD Region VI', 'Other']),
+            'date_ended' => fake()->optional(0.4)->dateTimeBetween($start, 'now')?->format('Y-m-d'),
+            'fund_source' => fake()->randomElement(['DSWD Central Office', 'LGU', 'DSWD Region VI', 'Other']),
         ];
     }
 }
